6c44f9894eed45d729700be30c7143c2c3e9c456
choi <chj5156@naver.com>
2020-08-10 11:08:59 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Hello{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello,hello friend");
+		System.out.println("myname is choi ");
+		String name;
+		Scanner yd3=new Scanner(System.in);
+		name=yd3.next();
+	}
+}
